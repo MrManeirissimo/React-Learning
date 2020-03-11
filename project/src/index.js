f342c848e5d348bcc3770802e1f6a38ecd092093
Lucas <lucas.andrieux@sequor.com.br>
2020-03-11 16:12:35 -0300

    Setup sample project

---
+++ project/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
+
+const sample_element = <h2>Hello world</h2>
+ReactDOM.render(sample_element, document.getElementById('root'));
