d33028e1535964b3a759cf4efaea2038569780d8
Lucas <lucas.andrieux@sequor.com.br>
2020-03-12 17:17:36 -0300

    New readme

--- project/src/components/clock/Clock.js
+++ project/src/components/clock/Clock.js
@@ -6,9 +6,20 @@ import Header from '../header'
 // When rendered expands the JSX elements and uses it's 'date' internal state to display time accurately
 // When disabled clears the timer
 
-/* Thanks to the setState() call, React knows the state has changed,
- and calls the render() method again to learn
-  what should be on the screen. 
+/*  Thanks to the setState() call, React knows the state has changed,
+    and calls the render() method again to learn
+    what should be on the screen. 
+*/
+
+/*  If the Clock component is ever removed from the DOM,
+    React calls the componentWillUnmount() lifecycle method
+    so the timer is stopped.
+*/
+
+/*  My on inference here, 'setState' might be using an undercover observer patern.
+    We can infer that as the variables have a way of inform the component of it's
+    changes so this objects 'render()' method may be placede again in the render
+    pipeline.    
 */
 
 // Turning function component into component class
@@ -16,6 +27,10 @@ import Header from '../header'
 class Clock extends React.Component{
     constructor(props){
         super(props);
+
+        // The constructor is the only place where we can
+        // assign the 'this.state'.
+        // IN ALL OTHER INSTANCES WE MUST USE 'setState({stateObject});' 
         this.state = {date: new Date()};
     }
 
